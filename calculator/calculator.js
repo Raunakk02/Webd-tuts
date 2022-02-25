5be940feefa6d4a5bd58288ce3b396c6a6a9a71e
raunak_k02 <raunakk728@gmail.com>
2022-02-25 23:11:42 +0530

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -14,6 +14,17 @@ app.post("/",function(req,res){
     res.send("The result is: "+(n1+n2));
 })
 
+app.get("/bmiCalculator",function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+})
+
+app.post("/bmiCalculator",function(req,res){
+    var h = Number(req.body.height);
+    var w = Number(req.body.weight);
+
+    res.send("Your BMI is: "+(w/(h*h)));
+})
+
 app.listen(3000,function(){
     console.log("Server running on port 3000");
 })
