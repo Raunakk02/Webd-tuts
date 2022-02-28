f89163a1a993ae12c7fa89c2bf2749e85d796053
raunak_k02 <raunakk728@gmail.com>
2022-02-28 18:05:34 +0530

    completed api

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // MONGODB
@@ -22,41 +22,114 @@ const articleSchema = mongoose.Schema({
     content: String
 });
 
-const Article = mongoose.model("Article",articleSchema);
+const Article = mongoose.model("Article", articleSchema);
 
 // TODO START
 
-app.get("/articles",function(req,res){
-    Article.find(function(err,articles){
-        if(err){
-            res.send(err);
-        }else{
-            res.send(articles);
-        }
-    });
-});
+////////////////////////////////// REQUESTS FOR ALL ARTICLES ////////////////////////////////////////
+app.route("/articles")
 
-app.post("/articles",function(req,res){
-    let t = req.body.title;
-    let c = req.body.content;
-    console.log(t, c);
+    .get(function (req, res) {
+        Article.find(function (err, articles) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(articles);
+            }
+        });
+    })
 
-    const article = new Article({
-        title: t,
-        content: c
+    .post(function (req, res) {
+        let t = req.body.title;
+        let c = req.body.content;
+        console.log(t, c);
+
+        const article = new Article({
+            title: t,
+            content: c
+        });
+
+        article.save(function (err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("Successfully saved the data!");
+            }
+        });
+    })
+
+    .delete(function (req, res) {
+        Article.deleteMany({}, function (err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("All the articles deleted successfully!");
+            }
+        })
     });
 
-    article.save(function(err){
-        if(err){
-            res.send(err);
-        }else{
-            res.send("Successfully saved the data!");
-        }
+////////////////////////////////////// REQUESTS FOR SPECIFIC ARTICLE //////////////////////////
+app.route("/articles/:articleTitle")
+
+    .get(function (req, res) {
+        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send("No articles found that match the given title");
+            }
+        });
+    })
+
+    .put(function (req, res) {
+        Article.replaceOne(
+            { title: req.params.articleTitle },
+            {
+                title: req.body.title,
+                content: req.body.content
+            },
+            function (err) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("Updated successfully");
+                }
+            }
+        );
+    })
+    
+    .patch(function(req,res){
+        Article.updateOne(
+            { title: req.params.articleTitle },
+            {
+                title: req.body.title,
+                content: req.body.content
+            },
+            function (err) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("Updated successfully");
+                }
+            }
+        );
+    })
+    
+    .delete(function(req,res){
+        Article.deleteOne(
+            { title: req.params.articleTitle },
+            function (err) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("Deleted successfully");
+                }
+            }
+        );
     });
-});
 
 // TODO END
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server running on port 3000");
 })
